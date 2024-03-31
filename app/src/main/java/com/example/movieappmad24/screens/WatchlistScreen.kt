60588de9d35b40b40492224b0f4d2832b9bbc6e7
BaharRaf <114486248+BaharRaf@users.noreply.github.com>
2024-03-31 23:03:34 +0200

    task3: Watchlist MovieList update

--- app/src/main/java/com/example/movieappmad24/screens/WatchlistScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/WatchlistScreen.kt
@@ -53,32 +53,67 @@ fun MovieList(movies: List<Movie>, paddingValues: PaddingValues,navController: N
 // Mock function to get movies - replace this with your actual data fetching logic
 fun getWatchlistMovies(): List<Movie> {
     return listOf(
-        Movie(
-            id = "1",
-            title = "Example Movie 1",
-            year = "2010",
-            genre = "Action, Adventure", // Genre as a single String
-            director = "Director 1",
-            actors = "Actor 1, Actor 2",
-            plot = "This is an example plot for Movie 1.",
-            images = listOf("https://example.com/movie1.jpg"), // List of image URLs
-            trailer = "https://example.com/trailer1",
-            rating = "PG-13"
-        ),
-        Movie(
-            id = "2",
-            title = "Example Movie 2",
+        Movie(id = "tt0499549",
+            title = "Avatar",
+            year = "2009",
+            genre = "Action, Adventure, Fantasy",
+            director = "James Cameron",
+            actors = "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+            plot = "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+            images = listOf("https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2ODQ3NjMyMl5BMl5BanBnXkFtZTcwODg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxOTEwNDcxN15BMl5BanBnXkFtZTcwOTg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg"),
+            trailer = "trailer_placeholder",
+            rating = "7.9"),
+
+        Movie(id = "tt0848228",
+            title = "The Avengers",
             year = "2012",
-            genre = "Drama, Thriller",
-            director = "Director 2",
-            actors = "Actor 3, Actor 4",
-            plot = "This is an example plot for Movie 2.",
-            images = listOf("https://example.com/movie2.jpg"),
-            trailer = "https://example.com/trailer2",
-            rating = "R"
+            genre = "Action, Sci-Fi, Thriller",
+            director = "Joss Whedon",
+            actors = "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
+            plot = "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity.",
+            images = listOf("https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0NjY0NzE4OTReQTJeQWpwZ15BbWU3MDczODg2Nzc@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE1MzEzMjcyM15BMl5BanBnXkFtZTcwNDM4ODY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMwMzM2MTg1M15BMl5BanBnXkFtZTcwNjM4ODY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ4NzM2Mjc5MV5BMl5BanBnXkFtZTcwMTkwOTY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc3MzQ3NjA5N15BMl5BanBnXkFtZTcwMzY5OTY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg"),
+            trailer = "trailer_placeholder",
+            rating = "8.1"),
+
+        Movie(id = "tt0816692",
+            title = "Interstellar",
+            year = "2014",
+            genre = "Adventure, Drama, Sci-Fi",
+            director = "Christopher Nolan",
+            actors = "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+            plot = "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+            images = listOf("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA3NTEwOTMxMV5BMl5BanBnXkFtZTgwMjMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMzQ5ODE2MzEwM15BMl5BanBnXkFtZTgwMTMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg4Njk4MzY0Nl5BMl5BanBnXkFtZTgwMzIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMzE3MTM0MTc3Ml5BMl5BanBnXkFtZTgwMDIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNjYzNjE2NDk3N15BMl5BanBnXkFtZTgwNzEyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg"),
+            trailer = "trailer_placeholder",
+            rating = "8.6"),
+
+        Movie(id = "tt0944947",
+            title = "Game of Thrones",
+            year = "2011 - 2018",
+            genre = "Adventure, Drama, Fantasy",
+            director = "N/A",
+            actors = "Peter Dinklage, Lena Headey, Emilia Clarke, Kit Harington",
+            plot = "While a civil war brews between several noble families in Westeros, the children of the former rulers of the land attempt to rise up to power. Meanwhile a forgotten race, bent on destruction, plans to return after thousands of years in the North.",
+            images = listOf("https://images-na.ssl-images-amazon.com/images/M/MV5BNDc1MGUyNzItNWRkOC00MjM1LWJjNjMtZTZlYWIxMGRmYzVlXkEyXkFqcGdeQXVyMzU3MDEyNjk@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BZjZkN2M5ODgtMjQ2OC00ZjAxLWE1MjMtZDE0OTNmNGM0NWEwXkEyXkFqcGdeQXVyNjUxNzgwNTE@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMDk4Y2Y1MDAtNGVmMC00ZTlhLTlmMmQtYjcyN2VkNzUzZjg2XkEyXkFqcGdeQXVyNjUxNzgwNTE@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNjZjNWIzMzQtZWZjYy00ZTkwLWJiMTYtOWRkZDBhNWJhY2JmXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNTMyMTRjZWEtM2UxMS00ZjU5LWIxMTYtZDA5YmJhZmRjYTc4XkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_SX1777_CR0,0,1777,999_AL_.jpg"),
+            trailer = "trailer_placeholder",
+            rating = "9.5")
+
         )
-        // Add more movies as needed
-    )
 }
 
 
